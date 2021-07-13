b30d3e980389a3b23b17e6ff36ea58cdeac5647e
Aiden Isaia <aidenisaia@gmail.com>
2021-07-12 18:21:39 -0700

    product function

--- calculator.js
+++ calculator.js
@@ -7,13 +7,18 @@ var app = new Vue({
       number1: 0,
       number2: 0,
       number3: 0,
-      total: null,
+      totalSum: null,
+      totalProduct: null,
     };
   },
   methods: {
     sum: function () {
       console.log("Adding the numbers.");
-      this.total = parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3);
+      this.totalSum = parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3);
+    },
+    product: function () {
+      console.log("Multipling the numbers.");
+      this.totalProduct = parseInt(this.number1) * parseInt(this.number2) * parseInt(this.number3);
     },
   },
 });
