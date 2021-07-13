843045434093996d71c72062c799544bb9d3c69f
Aiden Isaia <aidenisaia@gmail.com>
2021-07-13 16:13:10 -0700

    add words to list

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ var app = new Vue({
     return {
       message: "Hello from JavaScript!",
       message1: "Hello Vue!",
+      hidden: false,
+      todos: [
+        { text: "Learn JavaScript" },
+        { text: "Learn Vue" },
+        { text: "Build something awesome" },
+        { text: "Sleep" },
+      ],
+      words: ["tea", "apple", "albino"],
+      newWord: "",
     };
   },
   methods: {
     reverseMessage: function () {
       this.message = this.message.split("").reverse().join("");
+    },
+    seeMessage: function () {
+      console.log("Toggling hidden");
+      this.hidden = !this.hidden;
+    },
+    addWord: function () {
+      console.log("Adding word");
+      console.log(this.newWord);
+      this.words.push(this.newWord);
+      this.newWord = "";
     },
   },
 });
